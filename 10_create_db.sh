2557e245930aa9b46b7ee321e91256175073821d
Federico Ramallo <framallo@gmail.com>
2015-03-16 11:44:50 -0600

    wait for sql to be available and then create the database if doesn't exists

--- 10_create_db.sh
+++ 10_create_db.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-echo "creating database $DB_NAME"
+while ! exec 6<>/dev/tcp/${MYSQL_PORT_3306_TCP_ADDR}/${MYSQL_PORT_3306_TCP_PORT}; do
+  echo "Trying to connect to mysql"
+  sleep 1
+done
+  
 
+echo "creating database $DB_NAME"
 if [[ $DB_PASS != "" ]]; then
   echo "Creating database"
-  mysql -u $DB_USER -p$DB_PASS -h $MYSQL_PORT_3306_TCP_ADDR -e "create database $DB_NAME;"
+  mysql -u $DB_USER -p$DB_PASS -h $MYSQL_PORT_3306_TCP_ADDR -e "create database IF NOT EXISTS $DB_NAME;"
 fi
 
 exit 0
